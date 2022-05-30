b14dcd7c51d4b2cf8ebefeff0616425c24b766b3
NekokeCore <nekokecore@gmail.com>
2022-05-30 23:05:16 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Resources;
+
+#pragma warning disable CS8600 // 禁用将 null 字面量或可能为 null 的值转换为非 null 类型。
+namespace VideoCov // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean debug = false;
+            ResourceManager resManager = new ResourceManager(typeof(Resource1));
+            Console.WriteLine(resManager.GetString("banner"));
+            Console.WriteLine(resManager.GetString("info"));
+            Console.WriteLine(resManager.GetString("auther"));
+            if (args.Length == 0) {
+                Console.WriteLine("[信息] {0}",resManager.GetString("argsifempty"));
+            }
+            else
+            {
+                if (args[0].Equals("调试") || args[0].Equals("--v"))
+                {
+                    debug = true;
+                };
+
+                if (args[0].Equals("帮助") || args[0].Equals("-h"))
+                {
+                    Console.WriteLine(resManager.GetString("help"));
+                };
+
+                if (args[0].Equals("mkv转mp4") || args[0].Equals("-mkv2mp4"))
+                {
+                    Utilities.video.mkv2mp4("1","1");
+                };
+            } ;
+            if (debug)
+            {
+                Console.WriteLine(string.Format("[DEBUG]接收到了{0}个小可爱", args.Length));//Debug 便于调试，显示接受了几个参数
+                foreach (var item in args)//Debug 便于显示输入了什么参数
+                {
+                    Console.WriteLine(item);
+                }
+            };
+        }
+    }
+}
---
+++ Utilities/video.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace VideoCov.Utilities
+{
+    public class video
+    {
+        private void cmd(string source, string output)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe"; //运行程序
+            p.StartInfo.UseShellExecute = false; //不使用系统权限
+            p.StartInfo.RedirectStandardOutput = true; //输出信息
+            p.StartInfo.RedirectStandardError = true; //输出信息
+            p.StartInfo.CreateNoWindow = true; //不显示程序窗口
+            p.Start(); //启动
+
+            p.StandardInput.WriteLine("echo {0}",source + "&exit");
+            p.StandardInput.AutoFlush = true;
+
+            p.WaitForExit();
+            p.Close();
+
+            Console.WriteLine(p.StandardOutput.ReadToEnd());
+            Console.WriteLine(source);
+            Console.WriteLine(output);
+        }
+        public static void mkv2mp4(string source,string output)
+        {
+            var v = new video();
+            v.cmd(source, output);
+        }
+    }
+}
