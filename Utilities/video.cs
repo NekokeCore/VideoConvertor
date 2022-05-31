142375f9f191a9fe67668552890b36419317256e
NekokeCore <nekokecore@gmail.com>
2022-05-31 13:34:46 +0800

    Quite output

--- Utilities/video.cs
+++ Utilities/video.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics;
+using System.Configuration;
+using System.Collections.Specialized;
 
 namespace VideoCov.Utilities
 {
@@ -9,26 +11,31 @@ namespace VideoCov.Utilities
             try
             {
                 Process p = new Process();
-                p.StartInfo.FileName = @"D:\TOOLS_PATH\ffmpeg-2022-05-26-git-0dcbe1c1aa-full_build\bin\ffmpeg.exe"; //运行程序
+                p.StartInfo.FileName = ConfigurationManager.AppSettings.Get("FFMPEG_PATH"); //运行程序
                 p.StartInfo.Arguments = args[0].ToString() +" "+ source + " " + args[1].ToString()+ " " +output; 
                 p.StartInfo.UseShellExecute = false; //不使用系统权限
-                p.StartInfo.RedirectStandardOutput = false; //不输出信息
-                p.StartInfo.RedirectStandardError = false; //不输出错误信息
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden; //隐藏窗体
+                p.StartInfo.CreateNoWindow = true; //不显示新窗口
                 p.Start(); //启动
                 p.WaitForExit();
                 p.Close();
             }
-            catch (Exception ex)
+            catch (System.ComponentModel.Win32Exception exc)
             {
-                Console.WriteLine(ex);
-                throw;
+                Console.WriteLine("找不到ffmpeg的路径：" + exc);
+                return;
             }
         }
         public static void mkv2mp4(string source,string output)
         {
-            var v = new Video();
-            string[] args = {"-i", "-c:v copy -c:a aac" };
-            v.ffmpeg(source,output,args);
+            Console.WriteLine("[mkv转mp4] 已开始转码");
+            DateTime beforDT = System.DateTime.Now;
+                var v = new Video();
+                string[] args = {"-i", "-c:v copy -c:a aac" };
+                v.ffmpeg(source,output,args);
+            DateTime afterDT = System.DateTime.Now;
+            TimeSpan time = afterDT.Subtract(beforDT);
+            Console.WriteLine("[mkv转mp4] 转码结束，用时{0}",time);
         }
     }
 }
